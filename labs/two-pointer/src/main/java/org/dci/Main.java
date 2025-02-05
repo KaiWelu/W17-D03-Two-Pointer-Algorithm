5d4dda14b11f1b6b794bd50c3c7848fb6db0bab6
KaiWelu <kaiweluda@gmail.com>
2025-02-05 16:07:49 +0100

    adds problem3

--- labs/two-pointer/src/main/java/org/dci/Main.java
+++ labs/two-pointer/src/main/java/org/dci/Main.java
@@ -15,6 +15,22 @@ public class Main {
         System.out.println(Arrays.toString(reverseArray(problemOne)));
 
 
+        Integer[] problemTwo = {3, 2, 2, 3, 4, 5, 3};
+        System.out.println("Problem 2: ");
+        System.out.println("Input:");
+        System.out.println(Arrays.toString(problemTwo));
+        System.out.println("Output");
+        problemTwo.
+        System.out.println(Arrays.toString(problemTwo));
+
+        /* i know this is supposed to be a String but they are immutable in Java
+         so I skipped the conversion to array step */
+        String[] problemThree = {"I","c","e","C", "r", "E", "A", "m"};
+        System.out.println("Problem 3: ");
+        System.out.println("Input: ");
+        System.out.println(Arrays.toString(problemThree));
+        System.out.println("Output: ");
+        System.out.println(Arrays.toString(reverseVowels(problemThree)));
 
 
     }
@@ -34,8 +50,33 @@ public class Main {
         return  inputArray;
     }
 
-    public static String[] reverseVowels(String[] input) {
+    public static String[] reverseVowels(String[] inputArray) {
         ArrayList<String> vowels = new ArrayList<>(Arrays.asList("a","e","i","o","u", "A", "E","I", "O", "U"));
 
+        int left = 0;
+        int right = inputArray.length - 1;
+
+        while(left < right) {
+
+            if(!vowels.contains(inputArray[left])) {
+                left++;
+                continue;
+            }
+
+            if(!vowels.contains(inputArray[right])) {
+                right--;
+                continue;
+            }
+
+
+            String buffer = inputArray[right];
+            inputArray[right] = inputArray[left];
+            inputArray[left] = buffer;
+            left++;
+            right--;
+        }
+
+
+        return inputArray;
     }
 }
