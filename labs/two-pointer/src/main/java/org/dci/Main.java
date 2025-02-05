2e088487a80d89411adb1ea587af7d910eb2b8b8
KaiWelu <kaiweluda@gmail.com>
2025-02-05 15:34:25 +0100

    adds problem 1

--- labs/two-pointer/src/main/java/org/dci/Main.java
+++ labs/two-pointer/src/main/java/org/dci/Main.java
@@ -1,7 +1,41 @@
 package org.dci;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        String[] problemOne = {"h", "e", "l", "l", "o"};
+        System.out.println("Problem 1:");
+        System.out.println("Input: ");
+        System.out.println(Arrays.toString(problemOne));
+        System.out.println("Output: ");
+        System.out.println(Arrays.toString(reverseArray(problemOne)));
+
+
+
+
+    }
+
+    public static String[] reverseArray(String[] inputArray) {
+        int left = 0;
+        int right = inputArray.length - 1;
+
+        while(left < right) {
+            String buffer = inputArray[right];
+            inputArray[right] = inputArray[left];
+            inputArray[left] = buffer;
+            left++;
+            right--;
+        }
+
+        return  inputArray;
+    }
+
+    public static String[] reverseVowels(String[] input) {
+        ArrayList<String> vowels = new ArrayList<>(Arrays.asList("a","e","i","o","u", "A", "E","I", "O", "U"));
+
     }
 }
